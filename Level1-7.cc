0a3ad70d3037d17d1a76abacc7f75c052d61ef91
sangji <parksangji1109@gmail.com>
2021-05-01 21:03:19 +0900

    [Level1] 폰켓몬 - 찾아라 프로그래밍 마에스터

---
+++ Level1-7.cc
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int size = nums.size() / 2;
+
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());    
+
+    if(nums.size() >= size)
+        return size;
+    else
+        return nums.size();
+}
