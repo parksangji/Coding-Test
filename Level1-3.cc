400ac23784431fcf8a82f2867149e7b7e1d2211b
sangji <parksangji1109@gmail.com>
2021-05-01 19:18:15 +0900

    [Level1] 예산 - Summer/Winter Coding(~2018)

---
+++ Level1-3.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    
+    sort(d.begin(),d.end());
+    
+    for(int i=0; i<d.size(); i++)
+    {
+        budget -= d[i];
+        
+        if(budget <0)
+        {
+            break;
+        }
+        else
+        {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
