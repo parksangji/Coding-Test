79f1eb6911649ac6a5c9ff702e7c9cae04d4b699
sangji <parksangji1109@gmail.com>
2021-05-02 23:26:24 +0900

    [Level1] 문자열을 정수로 바꾸기

---
+++ Level1-16.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    if(s[0] == '-')  
+    {
+        answer =  stoi(s.substr(1));
+        answer = answer * (-1);
+    }
+    else
+    {
+        answer = stoi(s);
+    }
+    return answer;
+}
