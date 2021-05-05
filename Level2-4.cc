22a1e99384c5a4d543b4a988b1e564234e84848d
sangji <parksangji1109@gmail.com>
2021-05-05 11:51:58 +0900

    [Level2] 힙 - 더 맵게

---
+++ Level2-4.cc
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+    for(int i=0; i<scoville.size();i++)
+    {
+        pq.push(scoville[i]);
+    }
+
+    while(1)
+    {
+        if(pq.top() < K)
+        {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if(a+(b*2)<K and pq.empty()) return -1;
+            pq.push(a+(b*2));
+            answer++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return answer;
+}
