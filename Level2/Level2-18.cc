70b4b34f95897e11790caade9a61f798810303a5
sangji <parksangji1109@gmail.com>
2021-05-19 10:28:25 +0900

    [Level2] 2020 KAKAO BLIND RECRUITMENT -문자열 압축

--- Level2/Level2-18.cc
+++ Level2/Level2-18.cc
@@ -4,25 +4,27 @@ using namespace std;
 
 int solution(string s) {
     int answer = s.length();
-    
-    for(int i=1; i<s.length() /2 ; i++)
+
+    for(int i=1; i<=s.length() /2 ; i++)
     {
         int count = 1;
-        string temp = s.substr(0,i);
         string same_string_count;
+        string temp = s.substr(0,i);
+
         for(int j=i; j<s.length(); j+=i)
         {
             if(temp == s.substr(j,i)) count++;
             else
             {
                 if(count >1 ) same_string_count += to_string(count);
+
                 same_string_count += temp;
                 temp = s.substr(j,i);
                 count = 1;
             }
         }
-        
         if(count > 1) same_string_count += to_string(count);
+
         same_string_count += temp;
         answer = answer < same_string_count.length() ? answer : same_string_count.length();
     }
