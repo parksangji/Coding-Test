96cd77953e89040a583e12c163601e998bc7aabc
박상지 <parksangji1109@gmail.com>
2021-09-17 17:26:31 +0900

    [백준][이분탐색][실버1] 공유기 설치

--- BaekJoon/Binary_Search/이분탐색_실버1_2110.cc
+++ BaekJoon/Binary_Search/이분탐색_실버1_2110.cc
@@ -6,5 +6,32 @@ using namespace std;
 
 int main()
 {
-    
+    int home,wifi;
+    cin >> home >> wifi;
+    vector<int> homes(home);
+    for(int i=0; i<home; i++) cin >> homes[i];
+    sort(homes.begin(),homes.end());
+    int start = 1,end = homes.back(),answer = 0;
+    while(start <= end)
+    {
+        int mid = (start + end) /2;
+        int prev = 0, cnt = 1;
+
+        for(int i=1; i<homes.size(); i++)
+        {
+            if(homes[i] - homes[prev] >= mid)
+            {
+                prev = i;
+                cnt ++;
+            }
+        }
+        if(cnt >= wifi) 
+        {
+            start = mid + 1;
+            answer = mid;
+        }
+        else end  = mid -1;
+
+    }
+    cout << answer;
 }
